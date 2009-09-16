765cfe155fc2bb2e43bb0c3eecab4038a377cc3c
Martin Purschke <purschke@bnl.gov>
2009-09-16 15:49:24 -0400

    mlp - almost working version

--- rcdaq_actions.h
+++ rcdaq_actions.h
@@ -1,17 +1,20 @@
 #ifndef __RCDAQ_ACTIONS_H
 #define __RCDAQ_ACTIONS_H
 
-#define DAQ_BEGIN   101
-#define DAQ_END     102
-#define DAQ_OPEN    103
-#define DAQ_CLOSE   104
+#define DAQ_BEGIN         101
+#define DAQ_END           102
+#define DAQ_OPEN          103
+#define DAQ_CLOSE         104
 #define DAQ_SETFILERULE   105
 #define DAQ_FAKETRIGGER   106
 
-#define DAQ_DEVICE  110
+#define DAQ_DEVICE        110
 #define DAQ_LISTREADLIST  111
+#define DAQ_CLEARREADLIST 112
+#define DAQ_STATUS        113
+#define DAQ_FULLSTATUS    114
 
-#define DAQ_ELOG    120
+#define DAQ_ELOG          120
 
 #define DAQ_DEVICE_RANDOM    1001
 
