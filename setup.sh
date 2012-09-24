20ace2b0dfb1b0ac1a976dbcc21d32e7997853ad
Martin <purschke@bnl.gov>
2012-09-24 12:16:35 -0400

    added code to convert \-tcsh into an absolute path

--- setup.sh
+++ setup.sh
@@ -1,5 +1,30 @@
 #! /bin/sh
 
+
+# the following line identifies this script as one to be 
+# listed in the configuration GUI
+
+# --RCDAQGUI
+
+# lines starting with the "--SC" tag are short comments 
+# (can be only one, later ones override earlier ones)
+
+# --SC Top Level setup script with a random device
+
+
+# lines starting with the "--SC" tag are long comments 
+
+# --LC This setup script is part of the RCDAQ distribution. 
+# --LC It uses the random device (i.e. no particular hardware) 
+# --LC and can run on any machine to give RCDAQ a spin.
+# --LC It can serve as a template to develop your own setups.
+
+# we need the $0 as absolute path b/c we pass it on to a "file" device further down
+D=`dirname "$0"`
+B=`basename "$0"`
+MYSELF="`cd \"$D\" 2>/dev/null && pwd || echo \"$D\"`/$B"
+
+
 # remember this is an executable script. We
 # have the full power of a shell script at our
 # fingertips.
@@ -7,7 +32,7 @@
 rcdaq_client daq_clear_readlist
 
 # we add this very file to the begin-run event
-rcdaq_client create_device device_file 9 900 $0
+rcdaq_client create_device device_file 9 900 "$MYSELF"
 
 # make the first randown device trigger enabled
 rcdaq_client create_device device_random 1 1001 64 0 4096 1
@@ -23,7 +48,7 @@ rcdaq_client daq_set_maxbuffersize 16
 rcdaq_client daq_status -l
 
 
-ELOG=`which elog`
+ELOG=$(which elog 2>/dev/null)
 [ -n "$ELOG" ]  && rcdaq_client elog localhost 666 RCDAQLog
 
 
