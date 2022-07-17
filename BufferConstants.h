92b03313414b8c8353a423aa09841d265252c6aa
Martin Purschke <purschke@bnl.gov>
2022-07-17 19:33:00 -0400

    mlp - consolidating the buffer definitions into one header file

--- BufferConstants.h
+++ BufferConstants.h
@@ -6,9 +6,18 @@
 #define EOBLENGTH 4
 #define BUFFERSIZE (1024*1024)
 
-#define BUFFERMARKER      0xffffffc0
-#define GZBUFFERMARKER    0xfffffafe
-#define LZO1XBUFFERMARKER 0xffffbbfe
+#define DAQONCSFORMAT 0
+#define DAQPRDFFORMAT 1
+
+#define PRDFBUFFERHEADER 0xffffffc0;
+#define ONCSBUFFERHEADER 0xffffc0c0;
+
+#define BUFFERMARKER          0xffffffc0U
+#define ONCSBUFFERMARKER      0xffffc0c0U
+#define GZBUFFERMARKER        0xfffffafeU
+#define LZO1XBUFFERMARKER     0xffffbbfeU
+#define ONCSLZO1XBUFFERMARKER 0xffffbbc0U
+
 
 #define BUFFERBLOCKSIZE 8192
 
